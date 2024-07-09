7ceb926e2c23062e3b3d93b08f329ee6423c290f
Sandesh <sandeshboedhoe@gmail.com>
2024-07-09 19:18:40 +0200

    working on table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dividend Tracker</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -72,6 +72,70 @@
                 </li>
             </ul>
         </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Aandeel</th>
+                    <th scope="col">Totale waarde</th>
+                    <th scope="col">Tot geïnvesteerd</th>
+                    <th scope="col">Aantal</th>
+                    <th scope="col">GAK</th>
+                    <th scope="col">Dividenden</th>
+                    <th scope="col">Winst / Verlies</th>
+                    <th scope="col">Winst / Verlies (ong.)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">APPLE INC. - COMMON ST</th>
+                    {{-- <td class="d-flex justify-content-between">
+                        <div>€</div>
+                        <div>2533,34</div>
+                    </td> --}}
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">2533,34</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">1645,20</div>
+                        </div>
+                    </td>
+                    <td>
+                        10
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">114,43</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">12,56</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">€ 838,34</div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-between w-100">
+                            <span class="mr-auto">€</span>
+                            <div class="ml-auto">€ 838,34</div>
+                        </div>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
 </body>
 
 </html>
