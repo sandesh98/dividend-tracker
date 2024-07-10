551c05f6a07733b1baaff986b830da980a6356be
Sandesh <sandeshboedhoe@gmail.com>
2024-07-10 19:53:15 +0200

    added table in portfolio view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -73,69 +73,131 @@
             </ul>
         </div>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Aandeel</th>
-                    <th scope="col">Totale waarde</th>
-                    <th scope="col">Tot geïnvesteerd</th>
-                    <th scope="col">Aantal</th>
-                    <th scope="col">GAK</th>
-                    <th scope="col">Dividenden</th>
-                    <th scope="col">Winst / Verlies</th>
-                    <th scope="col">Winst / Verlies (ong.)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">APPLE INC. - COMMON ST</th>
-                    {{-- <td class="d-flex justify-content-between">
-                        <div>€</div>
-                        <div>2533,34</div>
-                    </td> --}}
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">2533,34</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">1645,20</div>
-                        </div>
-                    </td>
-                    <td>
-                        10
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">114,43</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">12,56</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">€ 838,34</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-between w-100">
-                            <span class="mr-auto">€</span>
-                            <div class="ml-auto">€ 838,34</div>
-                        </div>
-                    </td>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr class="text-nowrap">
+                        <th scope="col">Aandeel</th>
+                        <th scope="col">Totale waarde</th>
+                        <th scope="col">Tot geïnvesteerd</th>
+                        <th scope="col">Aantal</th>
+                        <th scope="col">GAK</th>
+                        <th scope="col">Dividenden</th>
+                        <th scope="col">Winst / Verlies</th>
+                        <th scope="col">Winst / Verlies (ong.)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="text-nowrap">
+                            APPLE INC. - COMMON ST
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">2533,34</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">1645,20</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap text-end">
+                            10
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">114,43</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">12,56</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto"><i
+                                        class="bi bi-arrow-up-right-circle-fill text-success"></i></span>
+                                <div class="ml-auto">€ 838,34</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto"><i
+                                        class="bi bi-arrow-up-right-circle-fill text-success"></i></span>
+                                <div class="ml-auto">€ 838,34</div>
+                            </div>
+                        </td>
+                    </tr>
 
-                </tr>
-            </tbody>
-        </table>
+                    <tr>
+                        <td class="text-nowrap">
+                            COCA COLA COMPANY
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">1950,90</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">1790,98</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap text-end">
+                            34
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">56,22</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto">€</span>
+                                <div class="ml-auto">80,47</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto"><i
+                                        class="bi bi-arrow-up-right-circle-fill text-success"></i></span>
+                                <div class="ml-auto">€ 160,12</div>
+                            </div>
+                        </td>
+                        <td class="text-nowrap">
+                            <div class="d-flex justify-content-between w-100 text-nowrap">
+                                <span class="mr-auto"><i
+                                        class="bi bi-arrow-up-right-circle-fill text-success"></i></span>
+                                <div class="ml-auto">€ 235,53</div>
+                            </div>
+                        </td>
+                    </tr>
+
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <td class="text-muted fs-6">Aantal (2)</td>
+                        <td class="text-muted text-end">4483,90</td>
+                        <td class="text-muted text-end">3435,98</td>
+                        <td class="text-muted text-end">45</td>
+                        <td class="text-muted text-end">170,65</td>
+                        <td class="text-muted text-end">93,03</td>
+                        <td class="text-muted text-end">994,46</td>
+                        <td class="text-muted text-end">1069,87</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
 </body>
 
 </html>
