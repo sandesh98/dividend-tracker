98617030ea7cdd05733dbbfa24c3e1456a11b64d
Sandesh <sandeshboedhoe@gmail.com>
2024-07-13 10:47:26 +0200

    added a transaction euro card

---
+++ resources/views/portfolio/components/transaction-card-eur.blade.php
@@ -0,0 +1,24 @@
+<div class="card bg-light border-0 px-4 py-3">
+    <div class="d-flex gap-3 flex-column">
+        <div class="d-flex flex-column gap-1">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex gap-2 align-items-center">
+                    <div class="dot dot-success"></div>
+                    <span class="fw-bolder fs-5">5 aandelen gekocht</span>
+                </div>
+                <div class="d-flex gap-2">
+                    <span>10 januari 2020</span>
+                    <span>19:00</span>
+                </div>
+            </div>
+            <div class="d-flex gap-2">
+                <span class="fw-semibold">€ 1045,23</span>
+                <span>(inclusief € 2,00 transactiekosten)</span>
+            </div>
+        </div>
+        <div class="d-flex gap-2">
+            <span class="fw-semibold text-muted">Kosten per aandeel</span>
+            <span class="fw-semibold text-muted">€ 250,12</span>
+        </div>
+    </div>
+</div>
--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -4,7 +4,7 @@
     @include('partials.header', ['header' => 'APPLE INC. - COMMON ST', 'subheader' => 'US0378331005'])
 
     <div class="row">
-        <div class="col-8">
+        <div class="col-lg-8">
 
             <nav class="nav nav-pills" id="nav-tab" role="tablist">
                 <a class="nav-link active rounded-pill" id="transactions" data-bs-toggle="tab"
@@ -19,41 +19,65 @@
             <div class="tab-content" id="nav-tabContent">
                 <div class="tab-pane fade show active" id="nav-transactions" role="tabpanel" aria-labelledby="transactions"
                     tabindex="0">
-
-                    <div class="card bg-light border-0 px-4 py-3">
-                        <div class="d-flex gap-3 flex-column">
-                            <div class="d-flex flex-column gap-1">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="d-flex gap-2 align-items-center">
-                                        <div class="dot dot-success"></div>
-                                        <span class="fw-bolder fs-5">5 aandelen gekocht</span>
-                                    </div>
-                                    <div class="d-flex gap-2">
-                                        <span>10 januari 2020</span>
-                                        <span>19:00</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex gap-2">
-                                    <span class="fw-semibold">€ 1045,23</span>
-                                    <span>(inclusief € 2,00 transactiekosten)</span>
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <div class="d-flex gap-2">
-                                    <span class="fw-semibold text-muted">Wisselkoers</span>
-                                    <span class="fw-semibold text-muted">€ 1,0000 = $ 1,0875</span>
-                                </div>
-                                <div class="d-flex gap-2">
-                                    <span class="fw-semibold text-muted">Kosten per aandeel</span>
-                                    <span class="fw-semibold text-muted">$ 230 = € 250,12</span>
-                                </div>
-                            </div>
+                    
+                    <div class="d-grid gap-3">
+                        <div class="col-auto">
+                            <span class="bg-dark text-white px-3 py-2 rounded date-badge">Januari 2024</span>
                         </div>
+                        @for ($i = 0; $i < 10; $i++)
+                            @include('portfolio.components.transaction-card-eur')   
+                        @endfor
                     </div>
+
                 </div>
                 <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"
                     tabindex="0">
                     De PP
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card sticky-top px-3 py-4">
+                <span class="fw-bold pb-3">Informatie</span>
+                <div class="d-flex justify-content-between">
+                    <span>Totale waarde</span>
+                    <span class="text-purple fw-semibold">€ 2533,34</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>Aantal aandelen</span>
+                    <span class="fw-semibold">10</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>GAK</span>
+                    <span class="fw-semibold">€ 104,54</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>Totaal geïnvesteerd</span>
+                    <span class="fw-semibold">€ 1645,20</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>Eerste transactie</span>
+                    <span class="fw-semibold">10-03-2021</span>
+                </div>
+
+                <span class="fw-bold pb-3 pt-4">Dividenden</span>
+                <div class="d-flex justify-content-between">
+                    <span>Dividend ontvangen bruto</span>
+                    <span class="fw-bold">€ 19,90</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>Dividendbelasting</span>
+                    <span class="fw-bold">- € 1,71</span>
+                </div>
+                <hr class="border opacity-80 my-2">
+                <div class="d-flex justify-content-between">
+                    <span>Dividend ontvangen netto</span>
+                    <span class="fw-bold">€ 12,19</span>
                 </div>
             </div>
         </div>
