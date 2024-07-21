96112b20ebbb84bfc7c4e2bf7dfda562ccf85636
Sandesh <sandeshboedhoe@gmail.com>
2024-07-21 16:54:55 +0200

    registered new transaction view

---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.master')
+
+@section('main')
+
+    @include('partials.header', ['header' => 'Transacties'])
+
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\DividendController;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PortfolioController::class, 'index'])->name('portfolio.index');
 Route::get('/show', [PortfolioController::class, 'show'])->name('portfolio.show');
 
 Route::get('/dividend', [DividendController::class, 'index'])->name('dividend.index');
+
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transaction.index');
