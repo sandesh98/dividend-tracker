150eca6cccd73dd135febcafc2cbea06f766eb8c
Sandesh <sandeshboedhoe@gmail.com>
2025-01-05 20:18:08 +0100

    still working on getTotalAmountInvested methode

--- app/Models/Trade.php
+++ app/Models/Trade.php
@@ -20,7 +20,6 @@ class Trade extends Model
     }
 
 
-
     public static function getAverageStockPrice($stock)
     {
         $averageStockPrice = self::getTotalAmoundInvested($stock) / self::getStockQuantity($stock);
@@ -34,7 +33,7 @@ class Trade extends Model
 
     public static function getStockQuantity($stock)
     {
-        $trades = self::where('product', 'LIKE', $stock)->whereNotNull('action')->get();
+        $trades = self::where('product', 'LIKE', 'MASTERCARD INC-CL.A')->whereNotNull('action')->get();
 
         $buy = $trades->filter(function ($item) {
             return $item->action === 'buy';
@@ -50,6 +49,36 @@ class Trade extends Model
     public static function getTotalAmoundInvested($stock)
     {
         $trades = self::where('product', 'LIKE', $stock)->get();
+
+        $groupedTrades = $trades->groupBy('order_id');
+
+        foreach ($groupedTrades as $trade) {
+            // Dit kan ook uit de loop gehaald worden volgens mij.
+            if ($trade->first()->currency === 'EUR') {
+                // ATM doet dit nog niks
+                $buy = $trades->filter(function ($item) {
+                    return $item->action === 'buy';
+                })->sum('total_transaction_value');
+        
+        
+                $sell = $trades->filter(function ($item) {
+                    return $item->action === 'sell';
+                })->sum('total_transaction_value');    
+                
+                return ($buy - $sell) / 100;
+            }
+
+            if ($trade->first()->currency === 'USD') {
+
+                $fx = (int) ($trade->pluck('fx')->filter()[0] * 10000) / 10000;
+                $transactioncost = $trade->firstWhere('description', 'LIKE' , 'DEGIRO Transactiekosten en/of kosten van derden')->total_transaction_value / 100;
+                $transaction = $trade->firstWhere('action', 'LIKE', 'buy')->total_transaction_value / 100;
+
+                return $transaction * (1 / $fx) + $transactioncost;                  
+            }
+        }
+
+
 
         $buy = $trades->filter(function ($item) {
             return $item->action === 'buy';
