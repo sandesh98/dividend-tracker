72f2b757da8c2a0804f8ae76f4f865ec1170d1b1
Sandesh <sandeshboedhoe@gmail.com>
2025-01-07 10:27:22 +0100

    added some notes

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -8,6 +8,11 @@ class TransactionController extends Controller
 {
     public function index()
     {
+        // transacties tonen
+            // - aankopen van aandelen
+            // - verkopen van aandelen
+            // - ontvangen dividenden
+            
         return view('transaction.index');
     }
 }
