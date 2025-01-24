ad416bf3986a85a5806d8160d11763dbc1796886
Sandesh <sandeshboedhoe@gmail.com>
2025-01-24 11:23:41 +0100

    when running artisan migrate:refresh --seed the stock will be updated automatically

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,5 +17,8 @@ class DatabaseSeeder extends Seeder
             ManualTransactionsSeeder::class,
             TradesSeeder::class,
         ]);
+
+        Artisan::call('stock:update');
+        Artisan::call('stock:price');
     }
 }
