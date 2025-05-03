edd863bc8173933fb1ad97ae168cf761c1b05021
Sandesh <sandeshboedhoe@gmail.com>
2025-05-03 14:32:52 +0200

    Rename manuals transactions to cash movement

--- database/seeders/CashMovementSeeder.php
+++ database/seeders/CashMovementSeeder.php
@@ -7,19 +7,22 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ManualTransactionsSeeder extends Seeder
+class CashMovementSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $transactions = Transaction::where('description', 'LIKE', 'iDEAL Deposit')
-                            ->orWhere('description', 'LIKE', 'flatex terugstorting')
-                            ->get();
+        $transactions = Transaction::query()
+            ->where('description', 'iDEAL Deposit')
+            ->orWhere('description', 'flatex terugstorting')
+            ->get();
+
+
 
         foreach($transactions as $transaction) {
-            DB::insert('INSERT INTO manual_transactions (date, time, description, currency, total_transaction_value, created_at, updated_at) VALUES (:date, :time, :description, :currency, :total_transaction_value, :created_at, :updated_at)', [
+            DB::insert('INSERT INTO cash_movements (date, time, description, currency, total_transaction_value, created_at, updated_at) VALUES (:date, :time, :description, :currency, :total_transaction_value, :created_at, :updated_at)', [
                 'date' => $transaction->date,
                 'time' => $transaction->time,
                 'description' => $transaction->description,
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TransactionSeeder::class,
             StockSeeder::class,
+            CashMovementSeeder::class,
 //            ManualTransactionsSeeder::class,
 //            TradesSeeder::class,
 //            DividendSeeder::class
