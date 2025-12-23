f10c69740df50624106e59a5bdf7ca0fc1b16c62
Sandesh Boedhoe <sandeshboedhoe@gmail.com>
2025-12-23 18:43:09 +0100

    Add test for currency cast

--- tests/Feature/Rules/CurrencyRuleTest.php
+++ tests/Feature/Rules/CurrencyRuleTest.php
@@ -5,9 +5,9 @@ namespace Tests\Feature\Rules;
 use App\Models\Casts\AsCurrency;
 use App\Value\CurrencyType;
 use Illuminate\Database\Eloquent\Model;
-use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\TestWith;
 use Tests\TestCase;
+use ValueError;
 
 class CurrencyRuleTest extends TestCase
 {
@@ -25,14 +25,50 @@ class CurrencyRuleTest extends TestCase
     }
 
     #[TestWith(['GBP'])]
+    #[TestWith([''])]
     public function testGetThrowsExceptionForInvalidValue(string $value): void
     {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("Invalid currency value for {$value}");
+        $this->expectException(ValueError::class);
+        $this->expectExceptionMessage(sprintf(
+            '"%s" is not a valid backing value for enum %s',
+            $value,
+            CurrencyType::class,
+        ));
 
         $cast = new AsCurrency();
         $model = new class () extends Model {};
 
         $cast->get($model, 'currency', $value, []);
+    }
+
+    #[TestWith(['EUR', CurrencyType::EUR])]
+    #[TestWith(['USD', CurrencyType::USD])]
+    public function testSetAcceptsCurrencyEnum(string $value, CurrencyType $expected): void
+    {
+        $cast = new AsCurrency();
+        $model = new class () extends Model {};
+
+        $this->assertSame(
+            $value,
+            $cast->set($model, 'currency', $expected, [])
+        );
+    }
+
+
+    #[TestWith(['GBP'])]
+    #[TestWith([''])]
+    public function testSetThrowsExceptionForInvalidString(string $value): void
+    {
+        $this->expectException(ValueError::class);
+        $this->expectExceptionMessage(sprintf(
+            '"%s" is not a valid backing value for enum %s',
+            $value,
+            CurrencyType::class,
+        ));
+
+        $cast = new AsCurrency();
+        $model = new class () extends Model {};
+
+        $cast->set($model, 'currency', $value, []);
     }
 }
